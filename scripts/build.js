5aa8ca9543f5fb1792829480e32ada78c0823b45
rodric <rodric.glaser@slickdeals.net>
2019-07-30 13:33:18 -0700

    rollup css

--- scripts/build.js
+++ scripts/build.js
@@ -5,6 +5,9 @@ import babel from 'rollup-plugin-babel'
 import postcss from 'rollup-plugin-postcss'
 import autoprefixer from 'autoprefixer'
 import commonjs from 'rollup-plugin-commonjs'
+import replace from 'rollup-plugin-replace'
+import node from 'rollup-plugin-node-resolve'
+import css from 'rollup-plugin-css-only'
 
 export default [{
   input: path.join(__dirname, '..', 'src', 'components', 'Datepicker.vue'),
@@ -24,8 +27,21 @@ export default [{
     }
   ],
   plugins: [
-    vue({css: true}),
+    node({
+      extensions: ['.js', '.jsx', '.vue']
+    }),
+    css(),
+    vue({
+      css: false,
+      compileTemplate: true,
+      template: {
+        isProduction: true,
+      }
+    }),
     postcss({plugins: [autoprefixer()]}),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
     commonjs(),
     babel({exclude: 'node_modules/**'})
   ]
@@ -34,7 +50,9 @@ export default [{
   output: {
     file: 'dist/vuejs-datepicker.min.js',
     format: 'umd',
-    name: 'vuejsDatepicker'
+    name: 'vuejsDatepicker',
+    exports: 'named',
+    sourcemap: false
   },
   plugins: [
     vue({css: true}),
